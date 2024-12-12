96001d73fdb8bac6f770fb5e47d34fe9d7b77773
river-bbc <river.honer@bbc.co.uk>
2024-12-12 21:44:32 +0000

    improve styles for journeys

--- app/vehicle/[vehicleId]/components/OnwardCallList/RelativeTime.tsx
+++ app/vehicle/[vehicleId]/components/OnwardCallList/RelativeTime.tsx
@@ -1,34 +1,34 @@
 const getRelativeMinutes = (
   scheduledTime: Date,
-  expectedTime: Date,
+  expectedTime: Date
 ): number => {
   return Math.round(
-    Math.ceil((expectedTime.getTime() - scheduledTime.getTime()) / 1000 / 60),
-  )
-}
+    Math.ceil((expectedTime.getTime() - scheduledTime.getTime()) / 1000 / 60)
+  );
+};
 
 const RelativeTime = ({
   scheduled,
   expected,
 }: {
-  scheduled: Date
-  expected: Date
+  scheduled: Date;
+  expected: Date;
 }) => {
-  const minutesDifference = getRelativeMinutes(scheduled, expected)
-  if (minutesDifference === 0) return <span>on time</span>
+  const minutesDifference = getRelativeMinutes(scheduled, expected);
+  if (minutesDifference === 0) return <span>On time</span>;
 
-  const absoluteDifference = Math.abs(minutesDifference)
-  const isEarly = minutesDifference < 0
+  const absoluteDifference = Math.abs(minutesDifference);
+  const isEarly = minutesDifference < 0;
   return (
     <span
       style={{
-        color: isEarly ? 'green' : 'red',
+        color: isEarly ? "green" : "#cc2323",
       }}
     >
-      {absoluteDifference} {absoluteDifference === 1 ? 'minute' : 'minutes'}{' '}
-      {isEarly ? 'early' : 'late'}
+      {isEarly ? "Early" : "Delayed"} {absoluteDifference}{" "}
+      {absoluteDifference === 1 ? "min" : "mins"}{" "}
     </span>
-  )
-}
+  );
+};
 
-export default RelativeTime
+export default RelativeTime;
