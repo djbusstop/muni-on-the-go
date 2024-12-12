43c711dd250369f17b9d7fb703451e517f54a46c
river-bbc <river.honer@bbc.co.uk>
2024-12-12 22:18:57 +0000

    smaller form

--- ui/SearchForm.tsx
+++ ui/SearchForm.tsx
@@ -15,7 +15,7 @@ const VehicleIdForm = () => {
     >
       <input
         name="itemId"
-        className="text-2xl appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mb-2"
+        className="text-xl appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mb-2"
         type="number"
         inputMode="numeric"
         pattern="[0-9]*"
@@ -26,7 +26,7 @@ const VehicleIdForm = () => {
       <button
         type="submit"
         value="vehicle"
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded text-xl"
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded text-lg"
         onClick={(event) => {
           event.preventDefault();
           formRef.current?.reportValidity();
@@ -41,7 +41,7 @@ const VehicleIdForm = () => {
       <button
         type="submit"
         value="stop"
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded text-xl"
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded text-lg"
         onClick={(event) => {
           event.preventDefault();
           formRef.current?.reportValidity();
