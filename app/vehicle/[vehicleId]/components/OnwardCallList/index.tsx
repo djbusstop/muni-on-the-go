d23ab29d4638fc482137a30824d3ded0e3c7ee4a
river-bbc <river.honer@bbc.co.uk>
2024-12-13 12:15:53 +0000

    remove list item component

--- app/vehicle/[vehicleId]/components/OnwardCallList/index.tsx
+++ app/vehicle/[vehicleId]/components/OnwardCallList/index.tsx
@@ -1,4 +1,7 @@
-import OnwardCallListItem from "./OnwardCallListItem";
+import dayjs from "dayjs";
+import ListItemLink from "@/ui/ListItemLink";
+import clsx from "clsx";
+import RelativeTime from "@/ui/RelativeTime";
 
 const OnwardCallList = ({
   onwardCalls,
@@ -8,12 +11,41 @@ const OnwardCallList = ({
   return (
     <ul className="flex flex-col list-none gap-4">
       {onwardCalls.map((onwardCall, index) => {
+        const expectedArrivalTime = dayjs(
+          onwardCall.ExpectedArrivalTime ||
+            onwardCall.ExpectedDepartureTime ||
+            onwardCall.AimedArrivalTime
+        );
+        const scheduledArrivalTime = dayjs(onwardCall.AimedArrivalTime);
+
         return (
-          <OnwardCallListItem
+          <ListItemLink
             key={onwardCall.StopPointRef}
-            onwardCall={onwardCall}
-            nextStop={index === 0}
-          />
+            href={`/stop/${onwardCall.StopPointRef}`}
+          >
+            {/* Row */}
+            <div className={clsx(["flex", "items-center", "gap-2"])}>
+              {/* Left col */}
+              <div className="flex flex-col flex-grow leading-relaxed">
+                {index === 0 && (
+                  <span className="text-secondary text-xs">Next stop</span>
+                )}
+                <h3 className="font-bold text-md">
+                  {onwardCall.StopPointName}
+                </h3>
+                <span className="text-xs">
+                  <RelativeTime
+                    scheduled={scheduledArrivalTime.toDate()}
+                    expected={expectedArrivalTime.toDate()}
+                  />
+                </span>
+              </div>
+              {/* Right col */}
+              <div className="text-lg shrink-0">
+                <span>{expectedArrivalTime.format("HH:mm")}</span>
+              </div>
+            </div>
+          </ListItemLink>
         );
       })}
     </ul>
