fd14d54bf77d9ef17bc0fe47f3d4aea6fd0b207c
river-bbc <river.honer@bbc.co.uk>
2024-12-15 12:08:57 +0000

    one search form

--- ui/SearchForm.tsx
+++ ui/SearchForm.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { redirect, RedirectType } from "next/navigation";
+import { notFound, redirect, RedirectType } from "next/navigation";
 import { useRef, useState } from "react";
 
 const VehicleIdForm = () => {
@@ -14,44 +14,30 @@ const VehicleIdForm = () => {
       onSubmit={(event) => event.preventDefault()}
     >
       <input
-        name="itemId"
-        className="text-xl appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+        className="text-xl appearance-none border border-gray-400 rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
         type="number"
         inputMode="numeric"
         pattern="[0-9]*"
         aria-label="Current bus or train number"
-        placeholder="Vehicle or Stop #"
+        placeholder="Stop or Vehicle #"
         onChange={(event) => setInputValue(event.target.value)}
       />
       <button
         type="submit"
-        value="vehicle"
         className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded text-lg"
         onClick={(event) => {
           event.preventDefault();
           formRef.current?.reportValidity();
           if (inputValue && typeof inputValue === "string") {
-            const vehicleId = parseInt(inputValue);
-            redirect(`/vehicle/${vehicleId}`, RedirectType.push);
+            const searchId = parseInt(inputValue);
+            if (inputValue.length === 4) {
+              redirect(`/vehicle/${searchId}`, RedirectType.push);
+            }
+            redirect(`/stop/${searchId}`, RedirectType.push);
           }
         }}
       >
-        Vehicle
-      </button>
-      <button
-        type="submit"
-        value="stop"
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded text-lg"
-        onClick={(event) => {
-          event.preventDefault();
-          formRef.current?.reportValidity();
-          if (inputValue && typeof inputValue === "string") {
-            const stopId = parseInt(inputValue);
-            redirect(`/stop/${stopId}`, RedirectType.push);
-          }
-        }}
-      >
-        Stop
+        Search
       </button>
     </form>
   );
