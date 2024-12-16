b220230faa91483fd31b9a3a1fae3660c059d6ec
river-bbc <river.honer@bbc.co.uk>
2024-12-16 13:31:46 +0000

    clearable

--- ui/DirectionPicker.tsx
+++ ui/DirectionPicker.tsx
@@ -24,19 +24,35 @@ const DirectionPicker = ({
 
   if (directions.length <= 1) return null;
 
+  const setDirection = (direction: Direction) => {
+    const searchParamsWithUpdatedDirection = {
+      ...searchParams,
+      direction: direction,
+    };
+    const redirectUrl =
+      pathName +
+      `?${Object.entries(searchParamsWithUpdatedDirection).map(
+        ([key, value]) => `${key}=${value}`
+      )}`;
+    router.push(redirectUrl);
+  };
+
+  const removeDirection = (direction: Direction) => {
+    const searchParamsWithoutDirection = Object.entries(searchParams).filter(
+      ([key]) => key !== direction
+    );
+    const redirectUrl =
+      pathName +
+      `?${Object.entries(searchParamsWithoutDirection).map(
+        ([key, value]) => `${key}=${value}`
+      )}`;
+    router.push(redirectUrl);
+  };
+
   return (
     <div className={clsx(["flex", "gap-1"])}>
       {directions.map((direction) => {
         const isActive = value === direction;
-        const searchParamsWithUpdatedDirection = {
-          ...searchParams,
-          direction: direction,
-        };
-        const redirectUrl =
-          pathName +
-          `?${Object.entries(searchParamsWithUpdatedDirection).map(
-            ([key, value]) => `${key}=${value}`
-          )}`;
         return (
           <button
             className={clsx([
@@ -55,39 +71,19 @@ const DirectionPicker = ({
             ])}
             key={direction}
             onClick={() => {
-              router.push(redirectUrl);
+              if (isActive) {
+                removeDirection(direction);
+              } else {
+                setDirection(direction);
+              }
             }}
           >
             {DirectionName[direction].charAt(0).toUpperCase() +
-              DirectionName[direction].slice(1)}
+              DirectionName[direction].slice(1)}{" "}
+            {isActive ? " X" : ""}
           </button>
         );
       })}
-      {value && (
-        <button
-          onClick={() => {
-            const searchParamsWithoutDirection = searchParams;
-            const redirectUrl =
-              pathName +
-              `?${Object.entries(searchParamsWithoutDirection).map(
-                ([key, value]) => `${key}=${value}`
-              )}`;
-            router.push(redirectUrl);
-          }}
-          className={clsx([
-            "text-xs",
-            "py-1",
-            "px-3",
-            "rounded-full",
-            "text-gray-800",
-            "border",
-            "border-gray-800",
-            "hover:bg-gray-200",
-          ])}
-        >
-          Clear
-        </button>
-      )}
     </div>
   );
 };
