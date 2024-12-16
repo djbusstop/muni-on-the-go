4da40856711d287ff7dec1e3e7c2a0b1383ee6cd
river-bbc <river.honer@bbc.co.uk>
2024-12-16 15:59:56 +0000

    x padding on alert

--- ui/Alert.tsx
+++ ui/Alert.tsx
@@ -2,7 +2,7 @@ import clsx from "clsx";
 
 const Alert = ({ label }: { label: string }) => {
   return (
-    <div className={clsx(["flex", "items-center", "gap-4"])}>
+    <div className={clsx(["flex", "items-center", "gap-4", "px-3"])}>
       <span className="text-3xl">⚠️</span>
       <p className="text-lg">{label}</p>
     </div>
