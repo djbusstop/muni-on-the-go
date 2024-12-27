9297ad081d823d5ebee9e39066cd2721618ec9bd
river-bbc <river.honer@bbc.co.uk>
2024-12-27 04:48:58 +0000

    align spacing

--- app/stop/nearby/page.tsx
+++ app/stop/nearby/page.tsx
@@ -30,7 +30,7 @@ export default async function Page() {
         </div>
       </header>
 
-      <section className="my-4">
+      <section className="my-3">
         <NearbyStopsList
           stops={stopsResponse.Contents.dataObjects.ScheduledStopPoint}
         />
