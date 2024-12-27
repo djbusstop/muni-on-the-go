e245a303673da3163d99bd91ccd730eabed5ca43
river-bbc <river.honer@bbc.co.uk>
2024-12-27 04:34:00 +0000

    bring back nearby vehicles

--- ui/SearchForm.tsx
+++ ui/SearchForm.tsx
@@ -24,27 +24,8 @@ const VehicleIdForm = () => {
         placeholder="Stop or Vehicle #"
         onChange={(event) => setInputValue(event.target.value)}
       />
-      <div className={clsx(["flex", "gap-2"])}>
-        <button
-          aria-label="Find nearby stops"
-          title="Find nearby stops"
-          className="bg-gray-100 hover:bg-gray-200 text-white font-bold p-2 border border-gray-400 rounded text-lg"
-          onClick={() => {
-            router.push("/stop/nearby");
-          }}
-        >
-          🚏
-        </button>
-        {/* <button
-          aria-label="Find nearby vehicles"
-          title="Find nearby vehicles"
-          className="bg-gray-100 hover:bg-gray-200 text-white font-bold p-2 border border-gray-400 rounded text-lg"
-          onClick={() => {
-            router.push("/vehicle/nearby");
-          }}
-        >
-          🚌
-        </button> */}
+
+      <div className="flex gap-2">
         <button
           type="submit"
           className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded text-lg flex-grow"
@@ -61,6 +42,28 @@ const VehicleIdForm = () => {
           }}
         >
           Search
+        </button>
+      </div>
+      <div className={clsx(["flex", "gap-2"])}>
+        <button
+          aria-label="Find nearby stops"
+          title="Find nearby stops"
+          className="bg-gray-100 hover:bg-gray-200 text-gray-400 p-2 border border-gray-400 rounded text-lg flex-grow"
+          onClick={() => {
+            router.push("/stop/nearby");
+          }}
+        >
+          🚏
+        </button>
+        <button
+          aria-label="Find nearby vehicles"
+          title="Find nearby vehicles"
+          className="bg-gray-100 hover:bg-gray-200 text-gray-400 p-2 border border-gray-400 rounded text-lg flex-grow"
+          onClick={() => {
+            router.push("/vehicle/nearby");
+          }}
+        >
+          🚌
         </button>
       </div>
     </form>
